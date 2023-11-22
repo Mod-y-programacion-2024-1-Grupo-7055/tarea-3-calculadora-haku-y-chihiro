faf51cfda137c0ed118413c6c486a6cc4c1706bb
EllieWilliams5 <122070780+EllieWilliams5@users.noreply.github.com>
2023-11-22 07:18:55 -0600

    Update NodoTangente.java

--- src/NodoTangente.java
+++ src/NodoTangente.java
@@ -1,4 +1,39 @@
-
+/**
+ * Clase que nos permite evaluar la Tangente en nuestra calculadora.
+ */
 public class NodoTangente extends NodoOperador {
 
+/**
+ * Constructor
+ *
+ * @param izq
+ */        
+  public NodoTangente(CompositeEA izq) {
+    // Dado que esta operación solo necesita un parametro,
+    // haremos que el hijo derecho sea nulo.
+    super(izq, null);
+    //Esto no estoy seguro aun.
+    precedence = 3;
+  }
+
+/**
+ * La evaluación del nodo, la tangente del hijo izquierdo.
+ * @return la tangente en radianes del hijo izquierdo.
+ */
+  @Override
+  public double evalua(){
+      // La manera de resolverlo por omisión seria por radianes.
+      return Math.tan(Math.toRadians(izq.evalua()));
+    }    
+}
+  
+
+
+
+
+
+
+
+
+  
 }
