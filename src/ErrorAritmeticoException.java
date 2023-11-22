e17faf08f5142f659b4280816d1fe80b08dae4d2
EllieWilliams5 <122070780+EllieWilliams5@users.noreply.github.com>
2023-11-22 09:06:13 -0600

    Update ErrorAritmeticoException.java

--- src/ErrorAritmeticoException.java
+++ src/ErrorAritmeticoException.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
  * @author Alejandro Hernández Mora <alejandrohmora@ciencias.unam.mx>
  */
 public class ErrorAritmeticoException extends Exception{
     
     /**
-     *
      * @param error
      */
     public ErrorAritmeticoException(String error) {
