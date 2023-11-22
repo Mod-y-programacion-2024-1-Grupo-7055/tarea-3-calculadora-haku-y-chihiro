8f6fed626b2fce1aa6d963af3015923850ac5fdd
EllieWilliams5 <122070780+EllieWilliams5@users.noreply.github.com>
2023-11-22 08:54:58 -0600

    Update ErrorDeSintaxisException.java

--- src/ErrorDeSintaxisException.java
+++ src/ErrorDeSintaxisException.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 /**
- *
  * @author Alejandro Hernández Mora <alejandrohmora@ciencias.unam.mx>
  */
 public class ErrorDeSintaxisException extends Exception {
 
     /**
-     *
      * @param error
      */
     public ErrorDeSintaxisException(String error) {
