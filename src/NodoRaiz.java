0c13b778a9b81bb69c3e13cc22d3f6694a4c8a98
EllieWilliams5 <122070780+EllieWilliams5@users.noreply.github.com>
2023-11-22 08:45:42 -0600

    Create NodoRaiz.java

---
+++ src/NodoRaiz.java
@@ -0,0 +1,27 @@
+/**
+ * Clase que nos permite evaluar la raiz en nuestra calculadora.
+ */
+
+public class NodoRaiz extends NodoOperador {
+
+    /**
+     * Constructor de Raiz.
+     * @param izq
+     */
+    public NodoRaiz(CompositeEA izq) {
+	// Dado que esta operación solo necesita un parametro,
+	// haremos que el hijo derecho sea nulo.
+        super(izq, null);
+	// Eso esta por verse
+        precedence = 2;
+    }
+
+    /**
+     * La evaluación del nodo, la raiz del hijo izquierdo.
+     * @return la raiz de nuestro hijo izquierdo.
+     */
+    @Override
+    public double evalua() {
+        return Math.sqrt(izq.evalua());
+    }
+}
