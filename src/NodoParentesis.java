dffc1797566e03b78c4bc2f12514c12a2763a01e
EllieWilliams5 <122070780+EllieWilliams5@users.noreply.github.com>
2023-11-22 08:17:05 -0600

    Update NodoParentesis.java

--- src/NodoParentesis.java
+++ src/NodoParentesis.java
@@ -1,31 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 /**
- *
  * @author Alejandro Hernández Mora <alejandrohmora@ciencias.unam.mx>
  */ 
 public class NodoParentesis extends NodoOperador {
 
     /**
-     *
+     * Constructor de los parentesis
      */
     public NodoParentesis() {
         super();
-        precedence = 2;
+        precedence = 3;
     }
 
     /**
-     *
-     * @return
+     * @throws UnsupportedOperationException debido a que los parantesis
+     * no tienen un valor numerico.
      */
     @Override
     public double evalua() {
-        throw new UnsupportedOperationException("No se puede evaluar un parentesis"); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("No se puede evaluar un parentesis");
     }
 
 }
