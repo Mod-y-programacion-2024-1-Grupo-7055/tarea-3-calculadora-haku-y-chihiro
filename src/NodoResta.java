2a2520a81ebdcf3e82d584834d3af3a5622446f6
EllieWilliams5 <122070780+EllieWilliams5@users.noreply.github.com>
2023-11-22 07:39:12 -0600

    Update NodoResta.java

--- src/NodoResta.java
+++ src/NodoResta.java
@@ -1,31 +1,27 @@
-
-
-/**
- * 
- * 
+/** 
  * @author Alejandro Hernández Mora <alejandrohmora@ciencias.unam.mx>
  */
 public class NodoResta extends NodoOperador {
-
+    
     /**
-     *
+     * Constructor
      * @param izq
      * @param der
      */
     public NodoResta(CompositeEA izq, CompositeEA der) {
         super(izq, der);
-        precedence=0;
+        precedence = 0;
     }
 
     /**
      * * La evaluación del nodo, resta la evaluación de los hijos izquierdo y derecho.
-     * @return
+     * @return El resultado de la resta entre nuestro hijo izquierdo menos el hijo derecho.
+     * Si el hijo izquierdo es nulo, regresaremos el valor negativo del hijo derecho.
      */
     @Override
     public double evalua() {
-        if (izq != null) {
-            return izq.evalua() - der.evalua();
-        }
-        return -der.evalua();
+        if (izq == null) 
+            return -der.evalua();
+        return izq.evalua() - der.evalua();
     }
 }
