73d4de5171681bb69f7764e58fa192e237a83c71
EllieWilliams5 <122070780+EllieWilliams5@users.noreply.github.com>
2023-11-22 15:50:50 -0600

    Update NodoValor.java

--- src/NodoValor.java
+++ src/NodoValor.java
@@ -1,13 +1,15 @@
 /**
  * Clase que nos modelará a los nodos que tienen un valor aritmético.
  */
+
+//Clase Terminada...
+
 public class NodoValor implements CompositeEA {
 
     double valor;
 
     /**
-     * Constructor    
-     *
+     * Constructor del valor.   
      * @param valor
      */
     public NodoValor(double valor) {
@@ -15,9 +17,8 @@ public class NodoValor implements CompositeEA {
     }
 
     /**
-     * La evaluación de estoso nodos comprende en únicamente devolver el valor
+     * La evaluación de estos nodos comprende en únicamente devolver el valor
      * que guardan.
-     *
      * @return el valor del nodo.
      */
     @Override
