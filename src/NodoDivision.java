277ff8ca5ed22260b090966870d6334affcd2d6f
EllieWilliams5 <122070780+EllieWilliams5@users.noreply.github.com>
2023-11-22 08:47:09 -0600

    Update NodoDivision.java

--- src/NodoDivision.java
+++ src/NodoDivision.java
@@ -1,30 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 /**
- *
  * @author Alejandro Hernández Mora <alejandrohmora@ciencias.unam.mx>
  */
 public class NodoDivision extends NodoOperador {
-
+    
     /**
-     *
+     * Constructor de la división.
      * @param izq
      * @param der
      */
     public NodoDivision(CompositeEA izq, CompositeEA der) {
         super(izq, der);
-        precedence=1;
+        precedence = 1;
     }
 
     /**
      * La evaluación del nodo, divide la evaluación de los hijos izquierdo y
      * derecho.
-     *
      * @return la división del hijo izquierdo entre el hijo derecho.
      */
     @Override
