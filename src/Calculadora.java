013bcba66420e79bef7aad512169c7acca92493b
EllieWilliams5 <122070780+EllieWilliams5@users.noreply.github.com>
2023-11-23 09:17:49 -0600

    Update Calculadora.java

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,20 +1,17 @@
-
 import java.util.StringTokenizer;
-
-
-
 /**
- *
+ * Clase main de nuestra calculadora.
  * @author Alejandro Hernández Mora <alejandrohmora@ciencias.unam.mx>
  */
+//CLASE FINAL
 public class Calculadora {
     static Compilador comp;
-   
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws ErrorDeSintaxisException {
-        String cadena = "(5-4)*3--2";
+        String cadena = "2 + 2";
         comp= new Compilador();
         StringTokenizer lexemas = comp.analisisLexico(cadena);
         CompositeEA nodo = comp.arbolDeAnalisisSintactico(lexemas);
