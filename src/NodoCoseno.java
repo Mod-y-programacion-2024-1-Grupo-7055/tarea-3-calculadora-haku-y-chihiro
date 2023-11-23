5d2d2cff4c42b2a257f732b7ef296d94ca12b87b
EllieWilliams5 <122070780+EllieWilliams5@users.noreply.github.com>
2023-11-22 22:30:33 -0600

    Update NodoCoseno.java

--- src/NodoCoseno.java
+++ src/NodoCoseno.java
@@ -1,27 +1,10 @@
-/**
- *Clase que nos permite evaluar el Coseno en nuestra calculadora.
- */
-
-//Clase terminada...
 public class NodoCoseno extends NodoOperador {
 
-  
-    /**
-     * Constructor del coseno
-     * @param izq
-     */
     public NodoCoseno(CompositeEA izq) {
-      	// Dado que esta operación solo necesita un parametro,
-	      // haremos que el hijo derecho sea nulo.
         super(izq, null);
-        // Eso esta por verse
         precedence = 2;
     }
 
-   /**
-    * La evaluación del nodo, el Coseno del hijo izquierdo.
-    * @return el coseno en radianes del hijo izquierdo.
-    */
     @Override
     public double evalua() {
         return Math.cos(Math.toRadians(izq.evalua()));
