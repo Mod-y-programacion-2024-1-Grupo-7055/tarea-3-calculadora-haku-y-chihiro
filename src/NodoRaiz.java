f06679328400b28d60737b4954aac384a2908137
EllieWilliams5 <122070780+EllieWilliams5@users.noreply.github.com>
2023-11-23 09:47:20 -0600

    Update and rename NodoRaizCuadrada.java to NodoRaiz.java

---
+++ src/NodoRaiz.java
@@ -0,0 +1,31 @@
+/**
+ * Clase que nos permite evaluar la raiz en nuestra calculadora.
+ */
+
+// CLASE TERMINADA.
+public class NodoRaiz extends NodoOperador {
+
+    /**
+     * Constructor de Raiz.
+     * @param der
+     */
+    public NodoRaizCuadrada(CompositeEA der) {
+	// Necesitamos que uno de los dos parametros sea nulo,
+        // pues solo requerimos uno para esta función.
+        // No estamos seguros de porque no funciona si hago null el
+        // hijo derecho, por eso hacemos null el hijo izq.
+        super(null, der);
+	// Mayor que las otra operaciones y menor que los parentesis.
+        precedence = 3;
+    }
+	
+     /**
+     * La evaluación del nodo. Resultado de evaluar el hijo
+     * derecho en la raiz "√".
+     * @return Valor de evaluar hijo der.
+     */
+    @Override
+    public double evalua() {
+        return Math.sqrt(der.evalua());
+    }
+}
